3025f53d2e8f124473f57278dd74617302b3f99c
Eli Rykoff <erykoff@stanford.edu>
2025-12-29 11:04:03 -0500

    Add correct iteration over data pages.

--- numparquet/numparquet.py
+++ numparquet/numparquet.py
@@ -7,11 +7,9 @@ from .encoding import NumpyBuffer, decode_data
 
 
 # TODO:
-#  * column name reading
-#  * plain buffer reading clarifications.
 #  * test memory usage
 #  * test speed
-#  * string handling
+#  * add fixed length byte arrays
 #  * derived types (what do they look like)
 #  * tests (!)
 #  * fsspec/open file handles.
@@ -42,10 +40,18 @@ def read_numparquet(filename, columns=None):
 
         schema = NumparquetSchema(file_metadata)
 
+        if columns is None:
+            read_columns = schema.columns
+        else:
+            read_columns = columns
+
         # Make the output data dictionary.
         # Skip if not in read list!
         data_dict = {}
         for column in schema.columns:
+            if column not in read_columns:
+                continue
+
             if schema[column].is_byte_array:
                 # String/byte columns need special handling.
                 data_dict[column] = None
@@ -65,62 +71,16 @@ def read_numparquet(filename, columns=None):
             rgslice = slice(row_group_index, row_group_index + row_group_rows)
 
             for col_group in row_group.columns:
-                # Skip if not in read list!
                 col_metadata = col_group.meta_data
                 codec = col_metadata.codec
                 name = col_metadata.path_in_schema[-1]
 
+                # Skip if not reading.
+                if name not in read_columns:
+                    continue
+
                 is_byte_array = schema[name].is_byte_array
 
-                dict_offset = col_metadata.dictionary_page_offset
-                data_offset = col_metadata.data_page_offset
-
-                has_dictionary_data = False
-                if dict_offset is not None:
-                    has_dictionary_data = True
-
-                    # We read in the dictionary page.
-                    file_buffer.seek(dict_offset)
-                    page_header = read_page_header(file_buffer)
-
-                    read_buffer = np.empty(page_header.compressed_page_size, dtype="S1")
-                    file_buffer.readinto(read_buffer)
-
-                    dict_value_buffer = np.empty(page_header.uncompressed_page_size, dtype="S1")
-                    decompress_into(codec, read_buffer, dict_value_buffer)
-
-                    dict_npbuffer = NumpyBuffer(dict_value_buffer)
-
-                    dict_values = decode_data(
-                        dict_npbuffer,
-                        page_header.dictionary_page_header.encoding,
-                        page_header.dictionary_page_header.num_values,
-                        schema_element=schema[name],
-                    )
-
-                if not has_dictionary_data:
-                    raise NotImplementedError("Need dictionary data at this moment.")
-
-                # Read the data page and uncompress it.
-                file_buffer.seek(data_offset)
-                page_header = read_page_header(file_buffer)
-
-                read_buffer = np.empty(page_header.compressed_page_size, dtype="S1")
-                file_buffer.readinto(read_buffer)
-
-                data_page_buffer = np.empty(page_header.uncompressed_page_size, dtype="S1")
-                decompress_into(codec, read_buffer, data_page_buffer)
-
-                # This is a useful container for operating on the decompressed
-                # data.
-                npbuffer = NumpyBuffer(data_page_buffer)
-
-                # 1. Repetition levels data.  Currently unsupported.
-                if len(col_metadata.path_in_schema) > 1:
-                    raise NotImplementedError("Repetition level not currently supported.")
-
-                # 2. Definition levels data.  Only for optional columns.
-                #    This tells which are NULL.
                 has_definition_data = False
                 if not schema[name].required:
                     has_definition_data = True
@@ -132,67 +92,135 @@ def read_numparquet(filename, columns=None):
                         if not schema[part].required:
                             max_definition_level += 1
 
-                    bit_width = int(np.ceil(np.log2(max_definition_level + 1)))
+                    definition_level_bit_width = int(np.ceil(np.log2(max_definition_level + 1)))
 
-                    definition_values = decode_data(
+                # Seek to the start of the data in this column group.
+                dict_offset = col_metadata.dictionary_page_offset
+                data_offset = col_metadata.data_page_offset
+                has_dictionary_data = False
+                if dict_offset is None:
+                    file_buffer.seek(data_offset)
+                else:
+                    has_dictionary_data = True
+                    file_buffer.seek(dict_offset)
+
+                # Loop until we have read all the data.
+                col_group_index = 0
+                while col_group_index < row_group_rows:
+
+                    if has_dictionary_data:
+                        # Note that only the first page can be a dictionary
+                        # page; we will have to reset this at the end of the
+                        # loop.
+
+                        page_header = read_page_header(file_buffer)
+                        read_buffer = np.empty(page_header.compressed_page_size, dtype="S1")
+                        file_buffer.readinto(read_buffer)
+
+                        dict_value_buffer = np.empty(page_header.uncompressed_page_size, dtype="S1")
+                        decompress_into(codec, read_buffer, dict_value_buffer)
+
+                        dict_npbuffer = NumpyBuffer(dict_value_buffer)
+
+                        dict_values = decode_data(
+                            dict_npbuffer,
+                            page_header.dictionary_page_header.encoding,
+                            page_header.dictionary_page_header.num_values,
+                            schema_element=schema[name],
+                        )
+
+                    # Read the data page and uncompress it.
+                    page_header = read_page_header(file_buffer)
+
+                    num_values_in_page = page_header.data_page_header.num_values
+
+                    read_buffer = np.empty(page_header.compressed_page_size, dtype="S1")
+                    file_buffer.readinto(read_buffer)
+
+                    data_page_buffer = np.empty(page_header.uncompressed_page_size, dtype="S1")
+                    decompress_into(codec, read_buffer, data_page_buffer)
+
+                    # This is a useful container for operating on the decompressed
+                    # data.
+                    npbuffer = NumpyBuffer(data_page_buffer)
+
+                    # 1. Repetition levels data.  Currently unsupported.
+                    if len(col_metadata.path_in_schema) > 1:
+                        raise NotImplementedError("Repetition level not currently supported.")
+
+                    # 2. Definition levels data.  Only for optional columns.
+                    #    This tells which are NULL.
+
+                    if has_definition_data:
+                        definition_values = decode_data(
+                            npbuffer,
+                            page_header.data_page_header.definition_level_encoding,
+                            num_values_in_page,
+                            bit_width=definition_level_bit_width,
+                            read_length=True,
+                        )
+
+                    # 3. Encoded values.
+
+                    if has_definition_data:
+                        # Only non-null entries are stored.
+                        data_value_count = np.sum(definition_values > 0)
+                    else:
+                        # All entries are stored.
+                        data_value_count = num_values_in_page
+
+                    null_count = num_values_in_page - data_value_count
+
+                    if has_dictionary_data:
+                        bit_width = int(npbuffer.read(1, dtype=np.uint8)[0])
+                    else:
+                        bit_width = None
+
+                    data_values = decode_data(
                         npbuffer,
-                        page_header.data_page_header.definition_level_encoding,
-                        page_header.data_page_header.num_values,
+                        page_header.data_page_header.encoding,
+                        data_value_count,
                         bit_width=bit_width,
-                        read_length=True,
+                        read_length=False,
+                        schema_element=schema[name],
                     )
 
-                # 3. Encoded values.
+                    if is_byte_array:
+                        if data_dict[name] is None:
+                            # First row group.
+                            data_dict[name] = np.empty(schema.num_rows, dtype=dict_values.dtype)
+                        else:
+                            # Subsequent row group. OR NOT FIX ME
+                            if dict_values.dtype.itemsize > data_dict[name].dtype.itemsize:
+                                # The strings got longer; we need to reallocate and
+                                # copy over the other data.
+                                temp = np.empty(schema.num_rows, dtype=dict_values.dtype)
+                                temp[0: row_group_index] = data_dict[name][0: row_group_index]
+                                data_dict[name] = temp
 
-                if has_definition_data:
-                    # Only non-null entries are stored.
-                    data_value_count = np.sum(definition_values > 0)
-                else:
-                    # All entries are stored.
-                    data_value_count = row_group_rows
-
-                null_count = row_group_rows - data_value_count
-
-                if has_dictionary_data:
-                    bit_width = int(npbuffer.read(1, dtype=np.uint8)[0])
-                else:
-                    bit_width = None
-
-                data_values = decode_data(
-                    npbuffer,
-                    page_header.data_page_header.encoding,
-                    data_value_count,
-                    bit_width=bit_width,
-                    read_length=False,
-                    schema_element=schema[name],
-                )
-
-                if is_byte_array:
-                    # Will need a non-dictionary example for the alternative.
-
-                    if data_dict[name] is None:
-                        # First row group.
-                        data_dict[name] = np.empty(schema.num_rows, dtype=dict_values.dtype)
+                    # Fill the output data.
+                    cgslice = slice(col_group_index, col_group_index + num_values_in_page)
+                    if has_dictionary_data:
+                        if has_definition_data and (null_count > 0):
+                            non_null = (definition_values > 0)
+                            data_dict[name][rgslice][cgslice][non_null] = dict_values[data_values]
+                            data_dict[name][rgslice][cgslice][~non_null] = schema[column].null_value
+                            data_dict[name].mask[rgslice][cgslice][~non_null] = True
+                        else:
+                            data_dict[name][rgslice][cgslice] = dict_values[data_values]
                     else:
-                        # Subsequent row group.
-                        if dict_values.dtype.itemsize > data_dict[name].dtype.itemsize:
-                            # The strings got longer; we need to reallocate and
-                            # copy over the other data.
-                            temp = np.empty(schema.num_rows, dtype=dict_values.dtype)
-                            temp[0: row_group_index] = data_dict[name][0: row_group_index]
-                            data_dict[name] = temp
+                        if has_definition_data and (null_count > 0):
+                            non_null = (definition_values > 0)
+                            data_dict[name][rgslice][cgslice][non_null] = data_values
+                            data_dict[name][rgslice][cgslice][~non_null] = schema[column].null_value
+                            data_dict[name].mask[rgslice][cgslice][~non_null] = True
+                        else:
+                            data_dict[name][rgslice][cgslice] = data_values
 
-                # Fill the output data.
-                if has_dictionary_data:
-                    if has_definition_data and (null_count > 0):
-                        non_null = (definition_values == 1)
-                        data_dict[name][rgslice][non_null] = dict_values[data_values]
-                        data_dict[name][rgslice][~non_null] = schema[column].null_value
-                        data_dict[name].mask[rgslice][~non_null] = True
-                    else:
-                        data_dict[name][rgslice] = dict_values[data_values]
-                else:
-                    raise NotImplementedError("Non-dictionary not implemented yet")
+                    # Increment the counter of number of rows read.
+                    col_group_index += num_values_in_page
+                    # Subsequent pages will not have dictionary data.
+                    has_dictionary_data = False
 
             row_group_index += row_group_rows
 
