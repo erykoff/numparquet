5998bcea9e04a948920313dd4ccb6e544be18c25
Eli Rykoff <erykoff@stanford.edu>
2026-01-01 17:10:38 -0800

    Fix flake8 stuff.

--- tests/simple_pyarrow_writer.py
+++ tests/simple_pyarrow_writer.py
@@ -1,11 +1,12 @@
 import numpy as np
+from math import prod
 
 try:
     import pyarrow as pa
     import pyarrow.parquet as pq
     # This is necessary for all pyarrow numpy functionality, unfortunately.
     # import pandas as pd
-except:
+except ImportError:
     raise ImportError("pyarrow not found for tests")
 
 
@@ -26,7 +27,9 @@ def numpy_dict_to_arrow_table(numpy_dict):
         dt = column.dtype
         if len(dt.shape) > 0:
             arrow_type = pa.list_(
-                pa.from_numpy_dtype(cast(tuple[np.dtype, tuple[int, ...]], dt.subdtype)[0].type),
+                # pa.from_numpy_dtype(np.dtype, dt.subdtype)[0].type
+                pa.from_numpy_dtype(dt.subdtype[0].type),
+                # pa.from_numpy_dtype(cast(tuple[np.dtype, tuple[int, ...]], dt.subdtype)[0].type),
                 prod(dt.shape),
             )
         elif dt.type == np.datetime64:
