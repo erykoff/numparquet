a40d984d3c79cfa33b17f0f686946103253be83c
Eli Rykoff <erykoff@stanford.edu>
2026-01-21 10:44:24 -0800

    Add compression tools.

--- numparquet/compression.py
+++ numparquet/compression.py
@@ -5,7 +5,7 @@ from .thrift import parquet_thrift
 
 def decompress_into(codec, in_buffer, out_buffer):
     """
-    Decompress from an input buffer to an output buffer, using codecs.
+    Decompress from an input buffer to an output buffer, using codec.
 
     Parameters
     ----------
@@ -34,3 +34,46 @@ def decompress_into(codec, in_buffer, out_buffer):
         zstd.decompress_into(in_buffer, out_buffer)
     else:
         raise NotImplementedError("Unsupported compression")
+
+
+def compress(codec, in_buffer):
+    """
+    Compress from an input butter, using codec.
+
+    Parameters
+    ----------
+    codec : `int`
+        Parquet codec code.
+    in_buffer : `np.ndarray` # NO
+    """
+    if codec == parquet_thrift.CompressionCodec.SNAPPY:
+        from cramjam import snappy
+
+        return snappy.compress_raw(in_buffer)
+    elif codec == parquet_thrift.CompressionCodec.UNCOMPRESSED:
+        # np.copyto(out_buffer, in_buffer, casting="no")
+        # out_buffer[:] = in_buffer
+        return in_buffer
+    elif codec == parquet_thrift.CompressionCodec.GZIP:
+        from cramjam import gzip
+
+        return gzip.compress(in_buffer)
+    elif codec == parquet_thrift.CompressionCodec.BROTLI:
+        from cramjam import brotli
+
+        return brotli.compress(in_buffer)
+    elif codec == parquet_thrift.CompressionCodec.ZSTD:
+        from cramjam import zstd
+
+        return zstd.compress(in_buffer)
+    else:
+        raise NotImplementedError("Unsupported compression")
+
+
+compression_string_map = {
+    "none": parquet_thrift.CompressionCodec.UNCOMPRESSED,
+    "snappy": parquet_thrift.CompressionCodec.SNAPPY,
+    "gzip": parquet_thrift.CompressionCodec.GZIP,
+    "brotli": parquet_thrift.CompressionCodec.BROTLI,
+    "zstd": parquet_thrift.CompressionCodec.ZSTD,
+}
